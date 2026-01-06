7e82a31d5e97dd4901cabb23007092e57ed6a5dd
Chris Osborn <c.osborn.contact@gmail.com>
2026-01-05 17:36:09 -0800

    Add initial scaffold and CI workflow

---
+++ examples/doc.go
@@ -0,0 +1,2 @@
+// Package examples will host runnable samples demonstrating how to use the Onyx Database Go SDK and CLIs.
+package examples
---
+++ onyx/doc.go
@@ -0,0 +1,3 @@
+// Package onyx provides the SDK implementation for interacting with the Onyx Database service.
+// All networked behavior, resolvers, and client wiring will be implemented here.
+package onyx
